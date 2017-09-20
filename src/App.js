16c9f939cb3fabcc725c36930bedff36e324b949
Garry <gtoborn@gmail.com>
2017-09-20 11:46:17 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <body>
+        <div className="App">
+          <div>
+
+          </div>
+          <div className="header-container">
+            <div className="navbar-logo-header">
+              <h2>Logo place</h2>
+            </div>
+            <div className="middle-nav-container">
+              <ul className="list-unstyled">
+                <li className="tsla-header-nav--list_link"><a href="#">Model S</a></li>
+                <li className="tsla-header-nav--list_link"><a href="#">Model X</a></li>
+                <li className="tsla-header-nav--list_link"><a href="#">Model 3</a></li>
+                <li className="tsla-header-nav--list_link"><a href="#">Energy</a></li>
+                <li className="tsla-header-nav--list_link sign-in"><a href="#">Sign In</a></li>
+              </ul>
+            </div>
+            <div className="header-right-nav-container">
+              <h3>right nav</h3>
+            </div>
+          </div>
+          <div className="main-nav-container">
+            <div className="title-container">
+              <h1>Quickest Acceleration.</h1>
+              <br />
+              <h1>Longest Range. The Safest Cars Ever.</h1>
+            </div>
+          </div>
+          <div className="promoContainer clearfix">
+            <ul className="list-inline">
+              <li className="col-sm-3"><a href="#">Link</a></li>
+              <li className="col-sm-3"><a href="#">Link</a></li>
+              <li className="col-sm-3"><a href="#">Link</a></li>
+              <li className="col-sm-3"><a href="#">Link</a></li>
+            </ul>
+          </div>
+          <div className="footer-container">
+            <div>
+              <ul className="list-unstyled">
+                <li className="footer-list"><a href="#">Tesla 2017</a></li>
+                <li className="footer-list"><a href="#">Privacy Legal</a></li>
+                <li className="footer-list"><a href="#">Contact</a></li>
+                <li className="footer-list"><a href="#">Careers</a></li>
+                <li className="footer-list"><a href="#">Forums</a></li>
+                <li className="footer-list"><a href="#">Locations</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </body>
+    );
+  }
+}
+
+export default App;
---
+++ src/middleNav.js
@@ -0,0 +1,38 @@
+<html>
+<style>
+    .promoContainer {
+        max-width: 900px;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    .promoContainer>ul li a {
+        background: rgba(0, 0, 0, 0.55);
+        display: block;
+        color: #fff;
+        padding: 10px;
+        text-transform: uppercase;
+        font-weight: bold;
+        transition: .25s ease all;
+        border: 1px solid transparent;
+    }
+
+    .promoContainer>ul li a:hover {
+        border: 1px solid #fff;
+    }
+
+
+</style>
+
+<div className="promoContainer clearfix">
+    <ul className="list-inline">
+        <li className="col-sm-3"><a href="#">Link</a></li>
+        <li className="col-sm-3"><a href="#">Link</a></li>
+        <li className="col-sm-3"><a href="#">Link</a></li>
+        <li className="col-sm-3"><a href="#">Link</a></li>
+    </ul>
+</div>
+
+
+
+</html>
